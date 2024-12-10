74391495712c8682df29f253cd9ef803da43e504
jjhorowitz1 <157649984+jjhorowitz1@users.noreply.github.com>
2024-12-09 19:07:08 -0500

    Update #i'm lit for my age.py

--- #i'm lit for my age.py
+++ #i'm lit for my age.py
@@ -3,7 +3,15 @@
 lit = True
 age = 21
 not_lit = 23
+sort_of_lit = False
+
 if lit:
     print(f"I am lit for my age ({age})")
 else:
     print(f"I am not lit for {age}")
+
+if sort_of_lit:
+    print(f"I am actually a great guy")
+else:
+    print(f"I am actually an ok guy")
+
